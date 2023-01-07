186a1fef61b0a99a8355d9d6d113d1dd3f24c2a8
Lucas Brum <brum.lucas@icloud.com>
2023-01-07 14:14:02 -0300

    Criando método para atualizar produto

--- src/main/java/br/com/bm/corretora/api/controller/ProdutoController.java
+++ src/main/java/br/com/bm/corretora/api/controller/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,6 +60,13 @@ public class ProdutoController {
         return ResponseEntity.created(uri).build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProdutoDTO> update(@PathVariable Long id, @Valid @RequestBody ProdutoDTO produtoDTO) {
+
+        Produto produto = produtoService.update(id, produtoDTO);
+
+        return ResponseEntity.ok().body(new ProdutoDTO(produto));
+    }
 
 
 }
